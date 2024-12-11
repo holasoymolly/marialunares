ec4b95227e1323ed4ff6cf654e6cee36ef383725
holasoymolly <hola@mollyyllom.com>
2024-12-11 14:40:37 -0400

    corregí tailwind.config.js y _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,15 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Meta adicional para asegurar compatibilidad */}
-        <meta charSet="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
-      </Head>
-      <body className="antialiased">
+      <Head />
+      <body>
         <Main />
         <NextScript />
       </body>
